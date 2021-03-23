9182d2b3c9cd8d00da422ac570221dddcc04a27e
Nixis198 <gager198@gmail.com>
2021-03-23 12:29:51 -0400

    Made the code look better :)

--- Program.cs
+++ Program.cs
@@ -6,43 +6,33 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Diagnostics;
 
-namespace ConsoleApp1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+namespace ConsoleApp1{
+    class Program{
+        static void Main(string[] args){
             Stopwatch time = new Stopwatch();
 
-            void treeGame(double num)
-            {
+            void treeGame(double num){
                 Console.WriteLine($"Tree for {num}:");
                 Console.WriteLine(num);
                 int steps = 0;
-                while(num != 1)
-                {
-                    if(num % 2 != 0)
-                    {
+                while(num != 1){
+                    if(num % 2 != 0){
                         num = 3 * num + 1;
                         steps++;
-                        if (num < 0)
-                        {
+                        if (num < 0){
                             Console.WriteLine($"{num} is weird");
                             break;
                         }
                         Console.WriteLine(num);
-                        if(num < 0)
-                        {
+                        if(num < 0){
                             Console.WriteLine($"{num} is weird");
                             break;
                         }
                     }
-                    else
-                    {
+                    else{
                         num = num / 2;
                         steps++;
-                        if (num < 0)
-                        {
+                        if (num < 0){
                             Console.WriteLine($"{num} is weird");
                             break;
                         }
@@ -50,27 +40,19 @@ namespace ConsoleApp1
                     }
                 }
                 Console.WriteLine($"Done in {steps} steps");
-                for(int i = 1; i <= 3; i++)
-                {
+                for(int i = 1; i <= 3; i++){
                     Console.WriteLine();
                 }
             }
 
-
-
-
             //////////////////////////////////////////////////////////////////////////// MAIN ////////////////////////////////////////////////////////////////////////////
-            
-
-            
 
             time.Start();
 
             double a = Math.Pow(2, 60);
             double b = Math.Pow(2, 64);
 
-            for(double i = a; i < b; i++)
-            {
+            for(double i = a; i < b; i++){
                 treeGame(i);
             }
 
